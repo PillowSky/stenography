abfebd6f64c389904566ec391cb23b151cbf20ae
Yuzo <hyzgog@gmail.com>
2015-11-20 03:07:09 +0800

    port as module

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var config = require('./config/config');
 var app = express();
 require('./config/express')(app, config);
 
-app.listen(config.port, function () {
-	console.log('Express server listening on port ' + config.port);
-});
+if (require.main == module) {
+	app.listen(config.port, function () {
+		console.log('Express server listening on port ' + this.address().port);
+	});
+} else {
+	module.exports.app = app;
+}
